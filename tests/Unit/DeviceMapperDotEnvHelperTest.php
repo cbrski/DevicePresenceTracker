baee33e50e9915d777beb3c02f2c927ef24d5252
Baranowski <baranowski@tuta.io>
2020-11-12 22:50:12 +0100

    added helper for getting defined names for devices from .env file

---
+++ tests/Unit/DeviceMapperDotEnvHelperTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\StorageBroker\Helpers\DeviceMapperDotEnvHelper;
+use Tests\TestCase;
+
+class DeviceMapperDotEnvHelperTest extends TestCase
+{
+    public function testGetHostnameByLladdr(): void
+    {
+        $_SERVER['OPENWRT_MAP_DEVICE_1'] = 'laptop1..11:11:11:11:11:11';
+        $_SERVER['OPENWRT_MAP_DEVICE_2'] = 'laptop2..21:11:11:11:11:11';
+
+        $hostname1 = DeviceMapperDotEnvHelper::getHostnameByLladdr('11:11:11:11:11:11');
+        $hostname2 = DeviceMapperDotEnvHelper::getHostnameByLladdr('21:11:11:11:11:11');
+
+        $this->assertEquals('laptop1', $hostname1);
+        $this->assertEquals('laptop2', $hostname2);
+    }
+}
