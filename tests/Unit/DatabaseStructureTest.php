c1f6ec036a1ddaf5007fe8cfb07c7af7ee09bf50
Baranowski <baranowski@tuta.io>
2020-11-12 18:14:29 +0100

    upgraded unit test

--- tests/Unit/DatabaseStructureTest.php
+++ tests/Unit/DatabaseStructureTest.php
@@ -3,8 +3,8 @@
 namespace Tests\Unit;
 
 use App\Device;
-use App\DeviceMac;
-use App\DeviceStateLog;
+use App\DeviceLink;
+use App\DeviceLinkStateLog;
 use App\Log;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -17,46 +17,71 @@ class DatabaseStructureTest extends TestCase
     {
         $device1 = new Device([
             'name' => 'laptop_1',
-            'ipv4' => ip2long('192.168.23.23'),
-            'dev' => 'eth10'
         ]);
         $device1->save();
 
-        $device_mac1 = new DeviceMac([
+        $device_link1 = new DeviceLink([
             'device_id' => $device1->id,
-            'mac' => '00:00:00:11:11:11',
-            'link_layer' => 'ethernet',
+            'lladdr' => '00:00:00:11:11:11',
+            'dev' => 'eth1',
+            'ipv4' => ip2long('192.168.100.100'),
+            'hostname' => 'computer',
         ]);
-        $device_mac1->save();
+        $device_link1->save();
 
-        $device_state_log1 = new DeviceStateLog([
+        $device_link_state_log1 = new DeviceLinkStateLog([
             'device_id' => $device1->id,
+            'device_link_id' => $device_link1->id,
+            'state' => DeviceLinkStateLog::STATE_REACHABLE,
             'timestamp' => time(),
         ]);
-        $device_state_log1->save();
+        $device_link_state_log1->save();
 
 
         $this->assertDatabaseHas(
             $device1->getTable(),
-            ['id' => $device1->id]
+            [
+                'id' => $device1->id,
+            ]
         );
         $this->assertDatabaseHas(
-            $device_mac1->getTable(),
-            ['device_id' => $device1->id]
+            $device_link1->getTable(),
+            [
+                'device_id' => $device1->id,
+                'hostname' => 'computer',
+            ]
         );
         $this->assertDatabaseHas(
-            $device_state_log1->getTable(),
-            ['device_id' => $device1->id]
+            $device_link_state_log1->getTable(),
+            [
+                'device_id' => $device1->id,
+                'device_link_id' => $device_link1->id,
+            ]
         );
 
 
         $this->assertDatabaseHas(
-            $device_mac1->getTable(),
-            ['id' => $device1->device_macs->first()->id]
+            $device_link1->getTable(),
+            ['id' => $device1->device_links->first()->id]
         );
         $this->assertDatabaseHas(
-            $device_state_log1->getTable(),
-            ['id' => $device1->device_state_logs->first()->id]
+            $device_link1->getTable(),
+            ['device_id' => $device1->device_link_state_logs->first()->id]
+        );
+        $this->assertDatabaseHas(
+            $device_link_state_log1->getTable(),
+            ['device_id' => $device_link_state_log1->device->id]
+        );
+        $this->assertDatabaseHas(
+            $device_link_state_log1->getTable(),
+            ['device_link_id' => $device1->device_links->first()->id]
+        );
+
+        $c = $device_link_state_log1->device_link->id;
+
+        $this->assertDatabaseHas(
+            $device_link_state_log1->getTable(),
+            ['device_link_id' => $device_link_state_log1->device_link->id]
         );
     }
 
@@ -65,15 +90,21 @@ class DatabaseStructureTest extends TestCase
         $ipv4['string'] = '192.168.50.40';
         $ipv4['int'] = ip2long($ipv4['string']);
 
-        $device1 = new Device([
-            'name' => 'laptop_1',
-            'ipv4' => ip2long($ipv4['string']),
-            'dev' => 'eth20',
-        ]);
+        $device1 = new Device(['name'=>'laptop_1']);
         $device1->save();
 
-        $this->assertEquals($ipv4['int'], $device1->ipv4);
-        $this->assertEquals(long2ip($device1->ipv4), $ipv4['string']);
+        $device_link1 = new DeviceLink([
+            'device_id' => $device1->id,
+            'name' => 'laptop_1',
+            'lladdr' => 'test',
+            'ipv4' => ip2long($ipv4['string']),
+            'dev' => 'eth20',
+            'hostname' => 'computer',
+        ]);
+        $device_link1->save();
+
+        $this->assertEquals($ipv4['int'], $device_link1->ipv4);
+        $this->assertEquals(long2ip($device_link1->ipv4), $ipv4['string']);
     }
 
     public function testWorkingLogTable()
