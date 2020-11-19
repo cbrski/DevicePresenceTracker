32afd631d6665a7eab32f998c267702884572abb
Baranowski <baranowski@tuta.io>
2020-11-19 11:36:27 +0100

    DeviceMapperDotEnvHelperTest: removed interference with other tests

--- tests/Unit/DeviceMapperDotEnvHelperTest.php
+++ tests/Unit/DeviceMapperDotEnvHelperTest.php
@@ -9,6 +9,9 @@ class DeviceMapperDotEnvHelperTest extends TestCase
 {
     public function testGetHostnameByLladdr(): void
     {
+        $originalMap1 = env('OPENWRT_MAP_DEVICE_1');
+        $originalMap2 = env('OPENWRT_MAP_DEVICE_2');
+
         $_SERVER['OPENWRT_MAP_DEVICE_1'] = 'laptop1..11:11:11:11:11:11';
         $_SERVER['OPENWRT_MAP_DEVICE_2'] = 'laptop2..21:11:11:11:11:11';
 
@@ -17,5 +20,8 @@ class DeviceMapperDotEnvHelperTest extends TestCase
 
         $this->assertEquals('laptop1', $hostname1);
         $this->assertEquals('laptop2', $hostname2);
+
+        $_SERVER['OPENWRT_MAP_DEVICE_1'] = $originalMap1;
+        $_SERVER['OPENWRT_MAP_DEVICE_2'] = $originalMap2;
     }
 }
