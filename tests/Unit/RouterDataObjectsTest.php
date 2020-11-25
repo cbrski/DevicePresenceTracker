e540d59857ebd64632aa87a841a01d63106b27b7
Baranowski <baranowski@tuta.io>
2020-11-25 11:11:19 +0000

    prepare test class for new test methods

--- tests/Unit/RouterDataObjectsTest.php
+++ tests/Unit/RouterDataObjectsTest.php
@@ -8,27 +8,56 @@ use Tests\TestCase;
 
 class RouterDataObjectsTest extends TestCase
 {
+    private array $rawNeighbour = [
+        [
+            'ip' => '10.11.11.11',
+            'hostname' => 'computer1',
+            'state' => DeviceLinkStateLog::STATE_FAILED,
+            'lladdr' => null,
+            'dev' => 'eth1',
+        ],
+        [
+            'ip' => '10.12.12.12',
+            'hostname' => 'computer2',
+            'state' => DeviceLinkStateLog::STATE_REACHABLE,
+            'lladdr' => '00:00:12:12:00:00',
+            'dev' => 'eth2',
+        ],
+        [
+            'ip' => '10.13.13.13',
+            'hostname' => 'computer3',
+            'state' => DeviceLinkStateLog::STATE_DELAY,
+            'lladdr' => '00:00:13:13:00:00',
+            'dev' => 'eth3',
+        ],
+    ];
+
+    private function getOneNeighbourFromRouter(int $i): \stdClass
+    {
+        $s = new \stdClass();
+        foreach ($this->rawNeighbour[$i] as $key => $val)
+        {
+            $s->{$key} = $val;
+        }
+        return $s;
+    }
+
+    private function getVal(int $i, string $key)
+    {
+        return $this->rawNeighbour[$i][$key];
+    }
+
     public function testNeighbour()
     {
-        $ip = '10.10.10.10';
-        $hostname = 'computer';
-        $state = DeviceLinkStateLog::STATE_FAILED;
-        $lladdr = null;
-        $dev = 'eth';
-
-        $s = new \stdClass();
-        $s->ip = $ip;
-        $s->hostname = $hostname;
-        $s->state = $state;
-        $s->lladdr = $lladdr;
-        $s->dev = $dev;
+        $s = $this->getOneNeighbourFromRouter(0);
 
         $n = new Neighbour($s);
-        $this->assertEquals($ip, $n->ip);
-        $this->assertEquals($hostname, $n->hostname);
-        $this->assertEquals($state, $n->state);
-        $this->assertEquals($lladdr, $n->lladdr);
-        $this->assertEquals($dev, $n->dev);
+        $this->assertEquals($this->getVal(0, 'ip'), $n->ip);
+        $this->assertEquals($this->getVal(0, 'hostname'), $n->hostname);
+        $this->assertEquals($this->getVal(0, 'state'), $n->state);
+        $this->assertEquals($this->getVal(0, 'lladdr'), $n->lladdr);
+        $this->assertEquals($this->getVal(0, 'dev'), $n->dev);
         $this->assertEquals(false, $n->thisDoesNotExist);
     }
+
 }
