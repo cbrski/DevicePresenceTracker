e265359cd7fbb448b8b7d98a089aa6ef406cbe7e
Baranowski <baranowski@tuta.io>
2020-12-16 13:00:53 +0000

    tests: added KeepersInstantiatorFromDatabase, VisibleDeviceKeeper, Helper needed for tests

---
+++ app/StorageBroker/Helpers/VisibleDeviceSynchronizator/Helper.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+
+namespace App\StorageBroker\Helpers\VisibleDeviceSynchronizator;
+
+
+use App\StorageBroker\Helpers\DeviceMapperDotEnvHelper;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Str;
+
+class Helper
+{
+    const UNDEFINED_DEVICE_NAME_PREFIX = '?_';
+    const UNDEFINED_DEVICE_NAME_LENGTH = 8;
+
+    private static function getHostnameByLladdr(string $lladdr): ?string
+    {
+        $deviceMapper = App::getFacadeRoot()->make(DeviceMapperDotEnvHelper::class);
+        return $deviceMapper->getHostnameByLladdr($lladdr);
+    }
+
+    public static function getNewNameForDevice(string $lladdr, string $hostname = null): string
+    {
+        $definedName = self::getHostnameByLladdr($lladdr);
+        $definedName = $definedName ?? $hostname;
+        $definedName = $definedName ?? self::UNDEFINED_DEVICE_NAME_PREFIX.strtolower(Str::random(
+                self::UNDEFINED_DEVICE_NAME_LENGTH - strlen(self::UNDEFINED_DEVICE_NAME_PREFIX)
+            ));
+        return $definedName;
+    }
+
+    public static function getUpdatedNameForDevice(string $lladdr, string $currentName): string
+    {
+        if (self::isRandomName($currentName)
+            && !is_null($newName = self::getHostnameByLladdr($lladdr))) {
+            return $newName;
+        }
+        return $currentName;
+    }
+
+    public static function isRandomName(string $name): bool
+    {
+        if (strlen($name) == self::UNDEFINED_DEVICE_NAME_LENGTH
+            &&
+            substr(
+                $name,
+                0,
+                strlen(self::UNDEFINED_DEVICE_NAME_PREFIX)
+            ) == self::UNDEFINED_DEVICE_NAME_PREFIX) {
+            return true;
+        }
+        return false;
+    }
+}
