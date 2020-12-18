21749b2698e883727676d7d1f3f3c77a8cafc42a
Baranowski <baranowski@tuta.io>
2020-12-18 09:16:11 +0100

    added __set() method to Neighbour

--- app/Api/Router/Structure/Neighbour.php
+++ app/Api/Router/Structure/Neighbour.php
@@ -13,26 +13,29 @@ class Neighbour
     private $hostname;
 
     public function __construct(\stdClass $rawNeighbour)
-{
-    foreach(['ip', 'dev', 'lladdr', 'state', 'hostname'] as $val)
     {
-        if (isset($rawNeighbour->{$val}))
-        {
-            $this->{$val} = $rawNeighbour->{$val};
-        }
-        else
-        {
-            $this->{$val} = null;
+        foreach(['ip', 'dev', 'lladdr', 'state', 'hostname'] as $val) {
+            if (isset($rawNeighbour->{$val}))  {
+                $this->{$val} = $rawNeighbour->{$val};
+            } else {
+                $this->{$val} = null;
+            }
         }
     }
-}
 
     public function __get($name)
-{
-    if (property_exists($this, $name))
     {
-        return $this->{$name};
+        if (property_exists($this, $name)) {
+            return $this->{$name};
+        }
+        return false;
     }
-    return false;
-}
+
+    public function __set($name, $value)
+    {
+        if (property_exists($this, $name)) {
+            $this->{$name} = $value;
+        }
+        return false;
+    }
 }
