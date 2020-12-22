e8ff70bc76f85ebd9ff8c1854168172891546c3f
Baranowski <baranowski@tuta.io>
2020-12-22 08:55:53 +0000

    clean code

--- app/StorageBroker/Helpers/VisibleDeviceSynchronizator/MatchMaker.php
+++ app/StorageBroker/Helpers/VisibleDeviceSynchronizator/MatchMaker.php
@@ -7,10 +7,8 @@ namespace App\StorageBroker\Helpers\VisibleDeviceSynchronizator;
 
 use App\Api\Router\Structure\Neighbour;
 use App\DeviceLinkStateLog;
-use App\Helpers\IpAddressInversion;
 use App\StorageBroker\Helpers\NeighboursRepository;
 use App\StorageBroker\Helpers\VisibleDevicesSynchronizator;
-use App\StorageBroker\Models\VisibleDevice;
 use Illuminate\Support\Collection;
 
 class MatchMaker
@@ -29,28 +27,21 @@ class MatchMaker
 
                     $kDl                = $keeper->getVisibleDevice()->getDeviceLink();
                     $keeperLladdr       = $kDl->lladdr;
-                    $keeperHostname     = $kDl->hostname;
                     $keeperIpv4Address  = $kDl->ipv4;
 
                     $isNullNeighbourLlladdr = is_null($neighbour->lladdr);
                     $isFailedNeighbourState = (0 == strcasecmp($neighbour->state, DeviceLinkStateLog::STATE_FAILED));
-                    $isEqualHostnames       = $neighbour->hostname == $keeperHostname;
                     $isEqualIpAddresses     = $neighbour->ip == $keeperIpv4Address;
                     $isEqualsLlladdrs       = $neighbour->lladdr == $keeperLladdr;
 
                     if (
                         ( $isNullNeighbourLlladdr
                             && $isFailedNeighbourState
-//                                && $isEqualHostnames )
                                 && $isEqualIpAddresses )
                         || $isEqualsLlladdrs
                     ) {
                         $keeper->setState(VisibleDevicesSynchronizator::INTERNAL_STATE_MATCHED);
                         $keeper->setNeighbour($neighbour);
-
-                        $deviceName = $keeper->getVisibleDevice()->getDevice()->name;
-                        $deviceLinkIP = $keeper->getVisibleDevice()->getDeviceLink()->ip;
-
                         unset($neighboursLeft[$keyNeighbour]);
                     }
                 }
