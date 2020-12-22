3e6e1aaf310211e7113539c463487803c25728a9
Baranowski <baranowski@tuta.io>
2020-12-22 08:15:45 +0000

    MatchMaker: bugfix of inproperly matching, use of Collection->map()

--- app/StorageBroker/Helpers/VisibleDeviceSynchronizator/MatchMaker.php
+++ app/StorageBroker/Helpers/VisibleDeviceSynchronizator/MatchMaker.php
@@ -6,30 +6,55 @@ namespace App\StorageBroker\Helpers\VisibleDeviceSynchronizator;
 
 
 use App\Api\Router\Structure\Neighbour;
+use App\DeviceLinkStateLog;
+use App\Helpers\IpAddressInversion;
 use App\StorageBroker\Helpers\NeighboursRepository;
 use App\StorageBroker\Helpers\VisibleDevicesSynchronizator;
+use App\StorageBroker\Models\VisibleDevice;
 use Illuminate\Support\Collection;
 
 class MatchMaker
 {
     public function match(Collection $keepers, NeighboursRepository $neighboursLeft): array
     {
-        /**
-         * @var int $keyNeighbour
-         * @var Neighbour $neighbour
-         * @var int $keyKeeper
-         * @var VisibleDeviceKeeper $keeper
-         * @var VisibleDeviceKeeper[] $keepers
-         */
-        foreach ($neighboursLeft as $keyNeighbour => $neighbour) {
-            foreach ($keepers as $keyKeeper => $keeper) {
-                if ($keeper->getVisibleDevice()->getDeviceLink()->lladdr == $neighbour->lladdr) {
-                    $keepers[$keyKeeper]->setState(VisibleDevicesSynchronizator::INTERNAL_STATE_MATCHED);
-                    $keepers[$keyKeeper]->setNeighbour($neighbour);
-                    unset($neighboursLeft[$keyNeighbour]);
+        $keepers->map(function ($keeper, $key) use ($neighboursLeft) {
+                /**
+                 * @var int $keyNeighbour
+                 * @var Neighbour $neighbour
+                 * @var int $keyKeeper
+                 * @var VisibleDeviceKeeper $keeper
+                 * @var VisibleDeviceKeeper[] $keepers
+                 */
+                foreach ($neighboursLeft as $keyNeighbour => $neighbour) {
+
+                    $kDl                = $keeper->getVisibleDevice()->getDeviceLink();
+                    $keeperLladdr       = $kDl->lladdr;
+                    $keeperHostname     = $kDl->hostname;
+                    $keeperIpv4Address  = $kDl->ipv4;
+
+                    $isNullNeighbourLlladdr = is_null($neighbour->lladdr);
+                    $isFailedNeighbourState = (0 == strcasecmp($neighbour->state, DeviceLinkStateLog::STATE_FAILED));
+                    $isEqualHostnames       = $neighbour->hostname == $keeperHostname;
+                    $isEqualIpAddresses     = $neighbour->ip == $keeperIpv4Address;
+                    $isEqualsLlladdrs       = $neighbour->lladdr == $keeperLladdr;
+
+                    if (
+                        ( $isNullNeighbourLlladdr
+                            && $isFailedNeighbourState
+//                                && $isEqualHostnames )
+                                && $isEqualIpAddresses )
+                        || $isEqualsLlladdrs
+                    ) {
+                        $keeper->setState(VisibleDevicesSynchronizator::INTERNAL_STATE_MATCHED);
+                        $keeper->setNeighbour($neighbour);
+
+                        $deviceName = $keeper->getVisibleDevice()->getDevice()->name;
+                        $deviceLinkIP = $keeper->getVisibleDevice()->getDeviceLink()->ip;
+
+                        unset($neighboursLeft[$keyNeighbour]);
+                    }
                 }
-            }
-        }
+        });
         return [$keepers, $neighboursLeft];
     }
 }
